9566eb7ea06c1e3568a259dccfc6dd071a6feb32
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2025-07-22 20:58:08 +0900

    집 컴 넘기기

--- echo.c
+++ echo.c
@@ -1,15 +1,27 @@
-#include <stdio.h>
-#include <syscall.h>
+#include <syscall.h>   /* syscall prototypes */
+#include <string.h>    /* strlen */
+#include <stdbool.h>   /* bool, true/false */
+#include <stdlib.h> // new -> EXIT_SUCCESS
 
 int
 main (int argc, char **argv)
 {
-  int i;
+  /* 파일 디스크립터 1은 STDOUT(표준 출력) */
+  const int fd = 1;
 
-  for (i = 0; i < argc; i++)
-    printf ("%s ", argv[i]);
-  printf ("\n");
+  for (int i = 1; i < argc; i++)
+    {
+      const char *s = argv[i];
+      size_t len = strlen (s);
+      /* 인자 문자열 출력 */
+      write (fd, s, len);
+      /* 마지막 인자가 아니면 공백 한 칸 출력 */
+      if (i + 1 < argc)
+        write (fd, " ", 1);
+    }
+
+  /* 줄 바꿈 */
+  write (fd, "\n", 1);
 
   return EXIT_SUCCESS;
 }
-
